f604512f169da2b183b9bcf590efe9ad1aea03c3
Kalicz Péter <kaliczp@gmail.com>
2025-03-28 17:58:23 +0100

    Precipitation import

---
+++ Import.R
@@ -0,0 +1,11 @@
+gridcoo <- read.table("gridpoints_coordinates.txt", head = TRUE)
+
+## Precipitation
+zipgrid <- unz("r_grid_19712023.txt.zip", "r_grid_19712023.txt")
+fejlec <- read.fwf(zipgrid, widths = rep(8, 1234), n = 1)
+alldata <- read.fwf(zipgrid, widths = rep(8, 1234), skip = 1)
+fejlec[1,1] <- "Date"
+names(alldata) <- fejlec
+JaszfelsoGrid <- alldata[,c("Date", "700")]
+GridDate <- seq.Date(from = as.Date("1971-01-01"), to = as.Date("2023-12-31"), by = "days")
+plot(GridDate, JaszfelsoGrid[,2], type = "l")
